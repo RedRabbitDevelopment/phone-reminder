bf7323ee2d7bb7669a89ad962d9fe1ecfa79399b
Nathan Tate <nathantate88@gmail.com>
2015-01-13 11:15:37 -0700

    Send reminder

--- scripts/send-reminder.js
+++ scripts/send-reminder.js
@@ -19,67 +19,31 @@ var SendReminderScript = module.exports = Starter.createScript({
       ]).spread(function(day, user) {
         if(user && (!day || !day.complete)) {
           var message, to, from;
-          var promises = [];
-          if(!day || !day.sentReminder) {
-            promises.push(_this.sendReminder(user));
+          var message, to, from;
+          switch(type) {
+            case 'sendReminder':
+              message = 'Just a reminder to lock up the church tonight! ' +
+                'When done, reply "Done" to this message. Any other reply to ' +
+                'this message will be forwarded on to Brother Caraway. Or you can ' +
+                'text him directly at ' + config.twilio.carawayNumber + '.';
+              to = user.phoneNumber;
+              break;
+            case 'sendExtraReminder':
+              message = 'Please don\'t forget to reply "Done" after you\'ve locked ' +
+                'up, otherwise you can expect a call from Brother Caraway.';
+              to = user.phoneNumber;
+              break;
+            case 'textCaraway':
+              message = 'It looks like no one closed up at the church today. Sorry.';
+              to = config.twilio.carawayNumber;
+              break;
+            default:
+              throw new Error('Unknown type "' + type + '".');
           }
-          if(!day || !day.sentExtraReminder) {
-            promises.push(_this.sendExtraReminder(user));
-          }
-          if(!day || !day.sentTextCaraway) {
-            promises.push(_this.textCaraway());
-          }
-          return Promise.all(promises);
+          from = config.twilio.adminNumber;
+          return twilio.sendMessage(from, to, message);
         };
       });
     });
-  },
-  sendReminder: function(user) {
-    var message, to, from;
-    var time = days.time();
-    if(time > 21) {
-      message = 'Just a reminder to lock up the church tonight! ' +
-        'When done, reply "Done" to this message. Any other reply to ' +
-        'this message will be forwarded on to Brother Caraway. Or you can ' +
-        'text him directly at ' + config.twilio.carawayNumber + '.';
-      to = user.phoneNumber;
-      from = config.twilio.adminNumber;
-      return Promise.all([
-        twilio.sendMessage(from, to, message),
-        this.setAsSent('sentReminder')
-      ]);
-    }
-  },
-  sendExtraReminder: function(user) {
-    var message, to, from;
-    var time = days.time();
-    if(time > 21.5) {
-      message = 'Please don\'t forget to reply "Done" after you\'ve locked ' +
-        'up, otherwise you can expect a call from Brother Caraway.';
-      to = user.phoneNumber;
-      from = config.twilio.adminNumber;
-      return Promise.all([
-        twilio.sendMessage(from, to, message),
-        this.setAsSent('sentExtraReminder')
-      ]);
-    }
-  },
-  textCaraway: function() {
-    var message, to, from;
-    var time = days.time();
-    if(time > 22) {
-      message = 'It looks like no one closed up at the church today. Sorry.';
-      to = config.twilio.carawayNumber;
-      from = config.twilio.adminNumber;
-      return Promise.all([
-        twilio.sendMessage(from, to, message),
-        this.setAsSent('sentTextCaraway')
-      ]);
-    }
-  },
-  setAsSent: function(name) {
-    var object = {};
-    object[name] = true;
-    return mongo.setToday(object);
   }
 });
