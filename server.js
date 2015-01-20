f576c46e9e9b5f837ea6f1d17fe57dd10e881ff5
Nathan Tate <nathantate88@gmail.com>
2015-01-20 11:27:08 -0700

    Fixed twilio response

--- server.js
+++ server.js
@@ -16,25 +16,27 @@ app.get('/keep-alive', function(req, res) {
 
 app.post('/message', bodyParser.urlencoded({extended: true}), function(req, res) {
   res.set('Content-Type', 'text/xml');
+  var response = '';
   var xml = {Response: {}};
   if(req.body.To && req.body.From && req.body.Body) {
     var incoming = mongo.log('incoming', req.body);
     if(req.body.Body.trim().match(/DONE(.|!)?/i)) {
-      xml.Response = 'Thanks!';
+      response = 'Thanks!';
       twilio.sendMessage(config.twilio.adminNumber, config.twilio.carawayNumber, 
         'The building security has been taken care of tonight!');
       mongo.setToday({complete: true}).done();
     } else if(req.body.Body.trim().match(/SUBSCRIBE(.|!)?/i)) {
       // To do: test the DB insert
       mongo.setOnDuty({phoneNumber: req.body.From}).done();
-      xml.Response = 'You have subscribed to secure the church building!';
+      response = 'You have subscribed to secure the church building!';
     } else {
-      xml.Response = 'Forwarding your message on to Brother Caraway. Please note that ' +
+      response = 'Forwarding your message on to Brother Caraway. Please note that ' +
         'if you are done, you should reply "Done" to this message (with no other text).';
       twilio.sendMessage(config.twilio.adminNumber, config.twilio.carawayNumber, 
         'Forwarded message from "' + req.body.From + '": "' + req.body.Body + '"');
     }
-    mongo.append(incoming, {response: xml.Response}).done();
+    mongo.append(incoming, {response: response}).done();
+    xml.Response.Message = response;
   }
   var builder = new xml2js.Builder();
   res.end(builder.buildObject(xml));
